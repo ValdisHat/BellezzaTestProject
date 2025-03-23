1fd7a7b03693b0804f3ca4a8e4f33c3e21dc7575
Felix <fagotmaster1@gmail.com>
2025-03-23 18:57:25 +0500

    Сообщение коммита

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,8 +3,9 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -600.0f;
+    
+	public const float Speed  = 300.0f;
+	public const float JumpVelocity  = -600.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
