4ff0cd5a156afaa566cc4e83b2a30d8646259d1d
Felix <fagotmaster1@gmail.com>
2025-03-28 16:36:02 +0500

    i have created an animation logic for the idle, walk and jump

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -4,13 +4,12 @@ using System.Security.Cryptography.X509Certificates;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed  = 300;
-	public const float JumpVelocity  = -600;
+	public const float Speed  = 400;
+	public const float JumpVelocity  = -700;
 	public const float Acceleration = 800;
 	private Vector2 velocity = Vector2.Zero;
 	
 	private AnimatedSprite2D _animatedSprite;
-	private AnimationPlayer _animationPlayer;
 	private AnimationTree _animationTree;
 	private AnimationNodeStateMachinePlayback _animationState;
 	private string _animationName;
@@ -19,7 +18,7 @@ public partial class Player : CharacterBody2D
 
 	public override void _Ready()
 	{
-		_animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		
 		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		_animationTree = GetNode<AnimationTree>("AnimationTree");
 		_animationTree.Active = true;
@@ -40,44 +39,46 @@ public partial class Player : CharacterBody2D
 			velocity += GetGravity() * (float)delta;	
 		}
 
-		if (Input.IsActionJustReleased("space") && velocity.Y < 0)
-		{
-				velocity.Y = JumpVelocity/43;
-		}
 		if (Input.IsActionJustPressed("space") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
+		if (Input.IsActionJustReleased("space") && Velocity.Y < 0)
+		{
+			velocity.Y = JumpVelocity/3;
+		}
+		
+		
 
 		float direction = Input.GetAxis("moveLeft", "moveRight");
 		if (direction != 0)
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, direction*Speed, Acceleration*(float)delta);
 			_animatedSprite.FlipH = velocity.X < 0; 	
-			if (IsOnFloor())
+			if (IsOnFloor() && Velocity.Y == 0)
 			{
 				_animationState.Travel("walk");
 			}	
 		}
 		else
 		{
-			_animationState.Travel("idle");
+			
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			if (IsOnFloor())
             {
-               
+               _animationState.Travel("idle");
             }
-			else
-			{
-				_animationState.Travel("jumpFall");
-			}
 		}
-
-
-		
+		if (isFalling)
+		{
+			_animationState.Travel("jumpFall");
+		}
+		if (isJumping)
+		{
+			_animationState.Travel("jumpStart");
+		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
-
 }
